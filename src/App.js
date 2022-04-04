5fcac2e3b1737b18622e6af2a20303abd0f300d3
wonkeunChoi <77766718+choiwk@users.noreply.github.com>
2022-04-04 13:03:05 +0900

    [Feat] Todo id를 통해 Detail 페이지에 대한 라우트 경로 설정

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './routes/Home';
 import CountNumber from './VanillaReduxNote/countNumber';
 import TodoList from './VanillaReduxNote/todoList';
+import Detail from './routes/Detail';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/:id" element={<Detail />} />
         <Route path="/count" element={<CountNumber />} />
         <Route path="todo" element={<TodoList />} />
       </Routes>
