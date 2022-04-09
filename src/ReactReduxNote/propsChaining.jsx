e8cfa4b3295c6ae71a6682fd8094e5955c521a43
wonkeunChoi <77766718+choiwk@users.noreply.github.com>
2022-04-09 20:46:54 +0900

    reducer 생성Provider,useSelector,useDispatch 정의

--- src/ReactReduxNote/propsChaining.jsx
+++ src/ReactReduxNote/propsChaining.jsx
@@ -1,75 +1,84 @@
-import React, { useState } from 'react';
+import { createStore } from 'redux';
+import { Provider, useSelector, useDispatch } from 'react-redux';
+
+//? Provider : reducer의 state를 어떤 컴포넌트들에게 전달하고 싶은가에 대한 설정.
+
+//? useSelector :reducer의 state의 값을 사용하고 싶을때 useSelector가 사용된다. 함수를 인자로 받는다.
+
+//? useDispatch : reducer의 state의 값을 변경하고 싶을때 action에게 전달하여 state의 값을 바꾸는데 사용된다.
+
 import '../ReactReduxNote/propsChaining.css';
 
-function PropsChaining() {
-  const [number, setNumber] = useState(1);
+function reducer(state, action) {
+  if (state === undefined) {
+    return { number: 1 };
+  }
+  // redux는 각각의 state의 변화를 불변하게 유지해야한다.
+  const newState = { ...state }; //새로운 state를 만드는데 과거의 state를 복제한다. 복제본을 수정하면 불변성을 유지할 수 있다.
+}
 
+const store = createStore(reducer);
+
+function PropsChaining() {
   return (
     <div id="container">
-      <h1>ROOT {number}</h1>
+      <h1>ROOT </h1>
       <div id="grid">
-        <Left1 number={number}></Left1>
-        <Right1 onIncrease={() => setNumber(number + 1)}></Right1>
+        <Left1></Left1>
+        <Right1></Right1>
       </div>
     </div>
   );
 }
 
-function Left1(props) {
+function Left1() {
   return (
     <div>
-      <h1>Left1 : {props.number}</h1>
-      <Left2 number={props.number}></Left2>
+      <h1>Left1 </h1>
+      <Left2></Left2>
     </div>
   );
 }
 
-function Left2(props) {
+function Left2() {
   return (
     <div>
-      <h1>Left2 : {props.number + 100}</h1>
-      <Left3 number={props.number}></Left3>
+      <h1>Left2 </h1>
+      <Left3></Left3>
     </div>
   );
 }
 
-function Left3(props) {
+function Left3() {
   return (
     <div>
-      <h1>Left3 : {props.number}</h1>
+      <h1>Left3 </h1>
     </div>
   );
 }
 
-function Right1(props) {
+function Right1() {
   return (
     <div>
-      <h1>Right1 : {props.number}</h1>
-      <Right2 onIncrease={() => props.onIncrease()}></Right2>
+      <h1>Right1 </h1>
+      <Right2></Right2>
     </div>
   );
 }
 
-function Right2(props) {
+function Right2() {
   return (
     <div>
-      <h1>Right2 : {props.number}</h1>
-      <Right3 onIncrease={() => props.onIncrease()}></Right3>
+      <h1>Right2 </h1>
+      <Right3></Right3>
     </div>
   );
 }
 
-function Right3(props) {
+function Right3() {
   return (
     <div>
-      <h1>Right3 : {props.number}</h1>
-      <input
-        type="button"
-        value="+"
-        onClick={() => {
-          props.onIncrease();
-        }}
-      ></input>
+      <h1>Right3 </h1>
     </div>
   );
 }
